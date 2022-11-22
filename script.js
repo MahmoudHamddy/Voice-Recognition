1bc57b260624d5e7f56879decfcac92ec1c74e77
sherif <sherifahmedelsayed2@gmail.com>
2022-11-22 03:20:19 +0200

    added a door

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 record_button = document.getElementById("recording_button");
 text_recorded = document.getElementById("text_recorded");
 said_text = document.getElementById("said_text");
+door = document.querySelector(".door");
+
+var isDoorOpen = false;
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -20,13 +23,22 @@ recognition.onresult = (event) => {
     
 
     if (text.toLowerCase().includes("open the door")) {
-        said_text.innerText = "DOOR OPENED"
+        // said_text.innerText = "DOOR OPENED"
+        if(!isDoorOpen){
+        door.classList.toggle("doorOpen");
+        isDoorOpen = true;
+        }
     }
     else if (text.toLowerCase().includes("close the door")){
-        said_text.innerText = "DOOR CLOSED"
+        // said_text.innerText = "DOOR CLOSED"
+        if (isDoorOpen) {
+            door.classList.toggle("doorOpen");   
+            isDoorOpen = false;
+        }
     }
     else{
         said_text.innerText = "Incorrect Password"
     }
     record_button.style.animationName = "";
   }
+
