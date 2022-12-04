f4dfbb8b1c3347ea14bafa72d24f5a10789eab5d
Basmaalaaeid <93398440+Basmaalaaeid@users.noreply.github.com>
2022-12-04 12:43:17 +0200

    Update server.py

--- server.py
+++ server.py
@@ -19,7 +19,6 @@ def upload_file():
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
 if __name__ == '__main__':
    app.run(debug=True)   
