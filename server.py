ac0953335cecc94e86e1df52ba7e11d7233a7098
Basmaalaaeid <93398440+Basmaalaaeid@users.noreply.github.com>
2022-12-04 10:56:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -11,5 +11,15 @@ app = Flask(__name__,template_folder="templates")
 def hello_name():
    return render_template('index.html')
 
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 if __name__ == '__main__':
    app.run(debug=True)   
