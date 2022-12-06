4dd418dd431014884f681c1fa60ff9518c9ba9d3
Yasmine Yasser <yasminyasser508@gmail.com>
2022-12-06 23:46:40 +0200

    adding the model

--- server.py
+++ server.py
@@ -2,23 +2,27 @@ from email import message
 from flask import Flask, redirect, url_for, request,render_template,flash,session
 import numpy as np
 from sympy import Id
+import pickle
+
 
 
 
 app = Flask(__name__,template_folder="templates")
+#model = pickle.load(open("DSP_Task3_TeamNo-main.rar", "rb"))
 
 @app.route('/')
 def hello_name():
    return render_template('index.html')
 
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
+# @app.route('/upload')
+# def upload_file():
+#    return render_template('upload.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       return 'file uploaded successfully'
+
 if __name__ == '__main__':
    app.run(debug=True)   
--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -12,6 +12,7 @@ recognition.lang = 'en-US';
 record_button.onclick = ()=>{
     record_button.style.animationName = "pulsating";
     recognition.start();
+    init();
 }
 
 recognition.onresult = (event) => {
@@ -19,7 +20,7 @@ recognition.onresult = (event) => {
     .map(result=>result[0])
     .map(result => result.transcript)
     .join("");
-    text_recorded.innerText = text.toUpperCase()
+    text_recorded.innerText = "You said: " + text.toUpperCase()
     
 
     if (text.toLowerCase().includes("open the door")) {
@@ -41,4 +42,60 @@ recognition.onresult = (event) => {
     }
     record_button.style.animationName = "";
   }
+
+
+// more documentation available at
+// https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/g19IVvfMD/";
+
+async function createModel() {
+    const checkpointURL = URL + "model.json"; // model topology
+    const metadataURL = URL + "metadata.json"; // model metadata
+
+    const recognizer = speechCommands.create(
+        "BROWSER_FFT", // fourier transform type, not useful to change
+        undefined, // speech commands vocabulary feature, not useful for your models
+        checkpointURL,
+        metadataURL);
+
+    // check that model and metadata are loaded via HTTPS requests.
+    await recognizer.ensureModelLoaded();
+
+    return recognizer;
+}
+
+//------------------------------------------------------------------------------------------------------
+async function init() {
+    const recognizer = await createModel();
+    const classLabels = recognizer.wordLabels(); // get class labels
+    const labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < classLabels.length; i++) {
+        labelContainer.appendChild(document.createElement("div"));
+    }
+    //const classPrediction="";
+    // listen() takes two arguments:
+    // 1. A callback function that is invoked anytime a word is recognized.
+    // 2. A configuration object with adjustable fields
+    recognizer.listen(result => {
+        const scores = result.scores; // probability of prediction for each class
+        const max_score = Math.max(...scores)
+        // render the probability scores per class
+        // for (let i = 0; i < classLabels.length; i++) {
+            // if (result.scores[i].toFixed(2)>=0.77){  
+        const classPrediction = classLabels[scores.indexOf(max_score)] + ": " + max_score.toFixed(2);
+        labelContainer.childNodes[scores.indexOf(max_score)].innerHTML = classPrediction;
+            // }    
+        // }
+    }, {
+        includeSpectrogram: true, // in case listen should return result.spectrogram
+        probabilityThreshold: 0.75,
+        invokeCallbackOnNoiseAndUnknown: true,
+        overlapFactor: 0.00 // probably want between 0.5 and 0.75. More info in README
+    });
+
+    // Stop the recognition in 5 seconds.
+    setTimeout(() => recognizer.stopListening(), 2000);
+}
 
